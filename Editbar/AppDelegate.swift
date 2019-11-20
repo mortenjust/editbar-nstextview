5582273dce43abd436ac6bb9b7134e1a7ffa46fb
Morten Just <morten.just@genstart.dk>
2019-11-20 17:06:04 +0100

    Initial Commit

---
+++ Editbar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Editbar
+//
+//  Created by Morten Just on 11/20/19.
+//  Copyright © 2019 Morten Just. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Editbar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Editbar
+//
+//  Created by Morten Just on 11/20/19.
+//  Copyright © 2019 Morten Just. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
